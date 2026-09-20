9d882acfe15703e13697cd5cf9b976265f5650d7
petrosasmamaw <petrosasmamaw2@gmail.com>
2026-09-20 23:50:12 +0300

    fix(client): eliminate DashboardSkeleton in ProtectedRoute and redirect to login while initializing

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,50 +1,11 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Navigate, useLocation } from 'react-router-dom'
-import { fetchSession } from '../features/auth/authSlice'
-
-function DashboardSkeleton() {
-  return (
-    <div className="min-h-screen page-parchment p-4">
-      <div className="container mx-auto">
-        <div className="mb-8">
-          <div className="skeleton h-4 w-24 rounded mb-2"></div>
-          <div className="skeleton h-8 w-48 rounded"></div>
-        </div>
-        <div className="grid md:grid-cols-3 gap-6 mb-8">
-          <div className="md:col-span-2 skeleton-card"></div>
-          <div className="skeleton-card"></div>
-        </div>
-        <div className="card">
-          <div className="skeleton h-6 w-40 rounded mb-4"></div>
-          <div className="space-y-3">
-            <div className="skeleton h-12 rounded"></div>
-            <div className="skeleton h-12 rounded"></div>
-            <div className="skeleton h-12 rounded"></div>
-            <div className="skeleton h-12 rounded"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 export default function ProtectedRoute({ children, requireAdmin = false }) {
   const { user, initializing } = useSelector((s) => s.auth)
   const location = useLocation()
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (initializing) {
-      dispatch(fetchSession())
-    }
-  }, [dispatch, initializing])
-
-  if (initializing) {
-    return <DashboardSkeleton />
-  }
-
-  if (!user) {
+  if (initializing || !user) {
     return <Navigate to="/login" replace state={{ from: location.pathname }} />
   }
 
